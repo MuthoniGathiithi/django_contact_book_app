8aefee381450f435a882deccd1acfdf5e73ae086
Joyce <gathiithijoyce74@gmail.com>
2025-07-20 20:32:08 +0300

    created models in contacts app

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+class Contact(models.Model):
+    name=models.CharField(max_length=10)
+    number=models.CharField(max_length=10)
+    email=models.EmailField()
+
 
 # Create your models here.
