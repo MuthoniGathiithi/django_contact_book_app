a6decb7fe72142b77e582885c21865cec87c46c6
Joyce <gathiithijoyce74@gmail.com>
2025-07-21 02:31:56 +0300

    modified contacts urls.py

--- contacts/urls.py
+++ contacts/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.view_contacts, name='view_contacts'),
     path('add/', views.add_contact, name='add_contact'),
-    path('update/<int:pk>/', views.update_contact, name='update_contact'),
-    path('delete/<int:pk>/', views.delete_contact, name='delete_contact'),
+    path('update/<int:contact_id>/', views.update_contact, name='update_contact'),
+    path('delete/<int:contact_id>/', views.delete_contact, name='delete_contact'),
 ]
