3fd178813cbff73940bae1e3db59471acc350f7a
Joyce <gathiithijoyce74@gmail.com>
2025-07-21 02:00:23 +0300

    updated views.py

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import ModelForm
+from django.contrib import messages
+from .models import Contact
 
-# Create your views here.
+def add_contact(request):
+   if request.method ==  'POST':
+         form = ModelForm(request.POST)
+         if form.is_valid():
+              form.save()
+              return redirect('view_contacts')
+         messages.success(request, "Contact added successfully!")
+   else:
+         form = ModelForm()
+   return render(request, 'contacts/add_contact.html', {'form': form})
+
+def view_contacts(request):
+    contacts= Contact.objects.all()
+    return render(request, 'contacts/view_contacts.html', {'contacts': contacts})
+
+def delete_contact(request, contact_id):
+    contact = Contact.objects.get(id=contact_id)
+    if request.method == 'POST':
+        contact.delete()
+        messages.success(request, "Contact deleted successfully!")
+        return redirect('view_contacts')
+    return render(request, 'contacts/delete_contact.html', {'contact': contact})
+
+def update_contact(request, contact_id):
+    contact = Contact.objects.get(id=contact_id)
+    if request.method == 'POST':
+        form = ModelForm(request.POST, instance=contact)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Contact updated successfully!")
+            return redirect('view_contacts')
+    else:
+        form = ModelForm(instance=contact)
+    return render(request, 'contacts/update_contact.html', {'form': form, 'contact': contact})
+
+         
