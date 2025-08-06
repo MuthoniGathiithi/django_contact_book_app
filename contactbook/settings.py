43fee4a86e3a74bc0a77710e47b7226a44e31944
Joyce <gathiithijoyce74@gmail.com>
2025-08-06 12:28:25 +0300

    modify files for production deployment

--- contactbook/settings.py
+++ contactbook/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r8by$ywl$s))ley0-*a1@j*07#n2tdj*_nkmvrqo7t3k4-6hvd'
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-r8by$ywl$s))ley0-*a1@j*07#n2tdj*_nkmvrqo7t3k4-6hvd')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -74,10 +78,11 @@ WSGI_APPLICATION = 'contactbook.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
@@ -115,7 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# WhiteNoise configuration
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- contacts/views.py
+++ contacts/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render,redirect
-from .forms import ModelForm
+from .forms import ContactForm
 from django.contrib import messages
 from .models import Contact
 
 def add_contact(request):
    if request.method ==  'POST':
-         form = ModelForm(request.POST)
+         form = ContactForm(request.POST)
          if form.is_valid():
               form.save()
               return redirect('view_contacts')
          messages.success(request, "Contact added successfully!")
    else:
-         form = ModelForm()
+         form = ContactForm()
    return render(request, 'contacts/add_contact.html', {'form': form})
 
 def view_contacts(request):
@@ -29,13 +29,13 @@ def delete_contact(request, contact_id):
 def update_contact(request, contact_id):
     contact = Contact.objects.get(id=contact_id)
     if request.method == 'POST':
-        form = ModelForm(request.POST, instance=contact)
+        form = ContactForm(request.POST, instance=contact)
         if form.is_valid():
             form.save()
             messages.success(request, "Contact updated successfully!")
             return redirect('view_contacts')
     else:
-        form = ModelForm(instance=contact)
+        form = ContactForm(instance=contact)
     return render(request, 'contacts/update_contact.html', {'form': form, 'contact': contact})
 
          
